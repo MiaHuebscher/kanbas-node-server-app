ca669f457e39425e801f728bf2494d62d2dc558e
Mia Huebscher <m.huebscher@att.net>
2024-06-12 12:01:18 -0700

    create collection schemas

--- Kanbas/Modules/schema.js
+++ Kanbas/Modules/schema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const modulesSchema = new mongoose.Schema({
     _id: { type: String, index: false},
-    name: { type: String, required: true, unique: true},
+    name: { type: String, required: true},
     description: String,
     course: {type: String, required: true},
     lessons: Array,
