ac854c61e000a611281fadf376de4cda507667d5
Mia Huebscher <m.huebscher@att.net>
2024-06-11 21:07:38 -0700

    connect MongoDB users collection to user interface

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
+import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import QuizRoutes from "./Kanbas/Quizzes/routes.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
@@ -14,6 +20,7 @@ CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 QuizRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen( process.env.PORT || 4000);
