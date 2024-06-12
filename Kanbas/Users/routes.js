273e87ff1f8663e1a19d8803dd90076410be46c5
Mia Huebscher <m.huebscher@att.net>
2024-06-12 10:40:54 -0700

    implement account controls

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,5 +1,4 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 export default function UserRoutes(app) {
   const createUser = async (req, res) => {
     const user = await dao.createUser(req.body);
@@ -39,19 +38,30 @@ export default function UserRoutes(app) {
       res.status(400).json(
         { message: "Username already taken" });
     }
-    currentUser = await dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
   const signin = async (req, res) => { 
     const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+    const currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session['currentUser'] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({message: "Username or Password Unable to be Found. Try again with different credentials or try again later."})
+    }
   };
   const signout = (req, res) => { 
-    currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
   const profile = async (req, res) => { 
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     res.json(currentUser);
   };
 
