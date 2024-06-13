49ee0bbbbb240a73606e142bcf66d43fc30d3f86
Mia Huebscher <m.huebscher@att.net>
2024-06-12 17:55:15 -0700

    connect app to MongoDB courses collection

---
+++ Kanbas/Courses/dao.js
@@ -0,0 +1,8 @@
+import model from "./model.js";
+
+export const findAllCourses = () => model.find();
+export const createCourse = (course) => {
+    return model.create(course);
+};
+export const deleteCourse = (courseId) => model.deleteOne({_id: courseId});
+export const updateCourse = (courseId, course) => model.updateOne({_id: courseId}, {$set: course});
--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,30 +1,30 @@
-import Database from "../Database/index.js";
+import * as dao from "./dao.js";
 
 export default function CourseRoutes(app) {
-    app.get("/api/courses", (req, res) => {
-        const courses = Database.courses;
-        res.send(courses);
-    });
+    const findAllCourses = async (req, res) => {
+        const courses = await dao.findAllCourses();
+        res.json(courses);
+    };
+    app.get("/api/courses", findAllCourses);
 
-    app.post("/api/courses", (req, res) => {
-        const course = { ...req.body,
+
+    const createCourse = async (req, res) => {
+        const newCourse = { ...req.body,
           _id: new Date().getTime().toString() };
-        Database.courses.push(course);
-        res.send(course);
-    }); 
+        const course = await dao.createCourse(newCourse);
+        res.json(course);
+    }; 
+    app.post("/api/courses", createCourse);
 
-    app.delete("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        Database.courses = Database.courses.filter((c) => c._id !== id);
-        res.sendStatus(204);
-    });
+    const deleteCourse = async (req, res) => {
+        const status = await dao.deleteCourse(req.params.id);
+        res.json(status);
+    };
+    app.delete("/api/courses/:id", deleteCourse);
     
-    app.put("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        const course = req.body;
-        Database.courses = Database.courses.map((c) =>
-          c._id === id ? { ...c, ...course } : c
-        );
-        res.sendStatus(204);
-    }); 
+    const updateCourse = async (req, res) => {
+        const status = await dao.updateCourse(req.params.id, req.body);
+        res.json(status);
+    };
+    app.put("/api/courses/:id", updateCourse);
 }
